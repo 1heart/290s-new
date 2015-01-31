752c8da57cee7bee4f0a116db4f1eb02cc913e80
Yixin Lin <yixin1996@gmail.com>
2015-01-31 15:39:24 -0500

    lol

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/home')
+def home():
+	return render_template('home')
+
 @app.route('/')
 def index():
     return render_template('index.html')
